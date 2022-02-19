949d48e8c4cd8d0dc0bdcd8e7b6df834f8021558
Luigi <luigicamastra@gmail.com>
2022-02-19 17:51:11 +0100

    ParserTimeStampPE.py: implemented arg parser

--- ParserTimeStampPE.py
+++ ParserTimeStampPE.py
@@ -1,22 +1,28 @@
-from pefile import *
+import pefile
 from pathlib import Path
 import datetime
-import time
 from os.path import exists
 import sys
-
+import argparse
 
 def main(folder):
+    parser = argparse.ArgumentParser(description="Parsing timestamp of PE file",epilog="Usage: ParserTimeStampPE.py -f <folder>")
+    parser.add_argument("folder",help="Parsing timestamp of PE file")
+    parser.add_argument("-f","--folder",help="Folder where are PE files")
+    args = parser.parse_args()
+    folder = args.folder
+
     if not exists(folder):
         print("Folder {} doesnt exist".format(folder))
         return 0
+
     for fn in Path(folder).glob("*.*"):
         try:
-            pe = PE(fn)
+            pe = pefile.PE(fn)
             print(fn,datetime.datetime.fromtimestamp(pe.FILE_HEADER.TimeDateStamp))
-        except PEFormatError as err:
+        except pefile.PEFormatError as err:
             print ("{} in file {}".format(err, fn))
             continue
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    main()
