b3cc0f233f9343f4f0f2ea197afa30700ebdceb2
Luigino Camastra <luigino.camastra@avast.com>
2022-02-25 00:53:08 +0100

    ParserTimeStampPE.py: sorting implemented

--- ParserTimeStampPE.py
+++ ParserTimeStampPE.py
@@ -4,10 +4,12 @@ import datetime
 from os.path import exists
 import sys
 import argparse
+from collections import OrderedDict
 
-def main(folder):
+peTimeStamps = {}
+
+def main():
     parser = argparse.ArgumentParser(description="Parsing timestamp of PE file",epilog="Usage: ParserTimeStampPE.py -f <folder>")
-    parser.add_argument("folder",help="Parsing timestamp of PE file")
     parser.add_argument("-f","--folder",help="Folder where are PE files")
     args = parser.parse_args()
     folder = args.folder
@@ -15,14 +17,20 @@ def main(folder):
     if not exists(folder):
         print("Folder {} doesnt exist".format(folder))
         return 0
-
+    w = open(folder + "\\" + "TimeStamp.txt", 'w')
+    
     for fn in Path(folder).glob("*.*"):
         try:
             pe = pefile.PE(fn)
-            print(fn,datetime.datetime.fromtimestamp(pe.FILE_HEADER.TimeDateStamp))
+            #print(fn,datetime.datetime.fromtimestamp(pe.FILE_HEADER.TimeDateStamp))
+            #w.write("{} {} \n".format(fn,datetime.datetime.fromtimestamp(pe.FILE_HEADER.TimeDateStamp)))
+            peTimeStamps[Path(fn).name] = datetime.datetime.fromtimestamp(pe.FILE_HEADER.TimeDateStamp)
         except pefile.PEFormatError as err:
             print ("{} in file {}".format(err, fn))
             continue
-
+    s = sorted(peTimeStamps,key = lambda x:peTimeStamps[x])
+    for i in s:
+       w.write("{}:{}\n".format(i,peTimeStamps[i]))
+    w.close()
 if __name__ == "__main__":
     main()
